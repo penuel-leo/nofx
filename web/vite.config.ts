b974ec8225a21dc25b114836bdecd03ab8fcb58d
penuel.li <penuel.li@kikitrade.com>
2025-10-31 11:11:27 +0800

    feat: test

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  },
   server: {
     port: 3000,
     proxy: {
